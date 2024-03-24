0e869a1d7ee7ede100660ebf52cbd12715ffb648
dfghjkcvbnmnbvcx <162885162+qwertyu34567@users.noreply.github.com>
2024-03-24 13:57:37 +0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+
+// Hàm đơn giản trả về kết quả là tổng của hai số
+function sum(a, b) {
+    return a + b;
+}
+
+// Xuất hàm để có thể sử dụng nó từ các module khác
+module.exports = { sum };
